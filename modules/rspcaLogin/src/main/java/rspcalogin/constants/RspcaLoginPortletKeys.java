b37118436ed2cb2226fe8f95f214f2bbf6b52754
Juan Paniagua <rpaniagua@digitalgeko.com>
2018-06-13 09:46:45 +0200

    Initial commit

---
+++ modules/rspcaLogin/src/main/java/rspcalogin/constants/RspcaLoginPortletKeys.java
@@ -0,0 +1,10 @@
+package rspcalogin.constants;
+
+/**
+ * @author juan
+ */
+public class RspcaLoginPortletKeys {
+
+	public static final String RspcaLogin = "RspcaLogin";
+
+}
---
+++ modules/rspcaLogin/src/main/java/rspcalogin/portlet/RspcaLoginPortlet.java
@@ -0,0 +1,29 @@
+package rspcalogin.portlet;
+
+import rspcalogin.constants.RspcaLoginPortletKeys;
+
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+
+import javax.portlet.Portlet;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author juan
+ */
+@Component(
+	immediate = true,
+	property = {
+		"com.liferay.portlet.display-category=category.sample",
+		"com.liferay.portlet.instanceable=true",
+		"javax.portlet.display-name=rspcaLogin Portlet",
+		"javax.portlet.init-param.template-path=/",
+		"javax.portlet.init-param.view-template=/view.jsp",
+		"javax.portlet.name=" + RspcaLoginPortletKeys.RspcaLogin,
+		"javax.portlet.resource-bundle=content.Language",
+		"javax.portlet.security-role-ref=power-user,user"
+	},
+	service = Portlet.class
+)
+public class RspcaLoginPortlet extends MVCPortlet {
+}
